60d9fc60a875235d501166af1c3e75bbc8a0e545
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-09-18 18:29:53 -0500

    removed the boarder around the block display

--- ui.py
+++ ui.py
@@ -28,15 +28,6 @@ def uiStart(block, lit_with_shadows_shader):
         scale=(0.2, 0.2, 0.2),
         texture=f'minecraft/model{block+1}.png',
     )
-
-    overlay = Entity(
-        parent=camera.ui,
-        model='cube',
-        color=color.white,
-        position=(0, -0.4),
-        scale=(0.6, 0.2, 0.2),
-        texture=f'minecraft/overlay.png',
-    )
     arm = Entity(
         parent=camera.ui,
         model='cube',
@@ -52,4 +43,4 @@ def uiStart(block, lit_with_shadows_shader):
         color=lerp(color.black, color.light_gray, 0.5),
         position=(1, 1)
     )
-    return blockIndecater1, blockIndecater2, blockIndecater3, arm, overlay, saveText
+    return blockIndecater1, blockIndecater2, blockIndecater3, arm, saveText
